071648843eaa068e90b6d77689a8a4aa7fbb5b24
zperee <elia.perenzin@bluewin.ch>
2020-11-09 19:13:10 +0100

    new flutter project

---
+++ lib/page/TabsView.dart
@@ -0,0 +1,34 @@
+import 'package:carpar_app/page/MapView.dart';
+import 'package:carpar_app/page/ParkingOverview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'CityView.dart';
+
+class TabsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.directions_car)),
+              Tab(icon: Icon(Icons.map)),
+              Tab(icon: Icon(Icons.star)),
+            ],
+          ),
+          title: Text('Zürich'),
+        ),
+        body: TabBarView(
+          children: [
+            ParkingOverview("2314234"),
+            MapView(),
+            MapView(),
+          ],
+        ),
+      ),
+    );
+  }
+}
